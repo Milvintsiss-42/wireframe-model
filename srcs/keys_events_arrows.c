b62742edbcc70a8aa1995874fd134596eb2123c1
Milvintsiss <milvintsiss@gmail.com>
2022-04-16 05:41:04 +0200

    Reverts screen translation directions to make it natural

--- srcs/keys_events_arrows.c
+++ srcs/keys_events_arrows.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 02:22:42 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/03/21 15:55:24 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/04/16 04:21:49 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,26 @@ void	ft_on_arrowl_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
 		ft_putstr("ARROW_LEFT key pressed \n");
-	fdf->draw.screentx -= fdf->draw.fscreen;
+	fdf->draw.screentx += fdf->draw.fscreen;
 }
 
 void	ft_on_arrowr_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
 		ft_putstr("ARROW_RIGHT key pressed \n");
-	fdf->draw.screentx += fdf->draw.fscreen;
+	fdf->draw.screentx -= fdf->draw.fscreen;
 }
 
 void	ft_on_arrowt_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
 		ft_putstr("ARROW_TOP key pressed \n");
-	fdf->draw.screenty -= fdf->draw.fscreen;
+	fdf->draw.screenty += fdf->draw.fscreen;
 }
 
 void	ft_on_arrowd_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
 		ft_putstr("ARROW_DOWN key pressed \n");
-	fdf->draw.screenty += fdf->draw.fscreen;
+	fdf->draw.screenty -= fdf->draw.fscreen;
 }
