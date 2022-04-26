6f8e8bd5168304eed14dfaeefa734e4a112fd4de
Milvintsiss <milvintsiss@gmail.com>
2022-04-26 03:44:05 +0200

    Corrects axis mode name in debug infos

--- srcs/key_modes_axis.c
+++ srcs/key_modes_axis.c
@@ -6,7 +6,7 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 16:36:47 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/03/21 16:37:45 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/04/26 03:43:46 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 static void	ft_on_axis_x_mode_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
-		ft_putstr("ROTATE_X MODE key pressed \n");
+		ft_putstr("AXIS_X MODE key pressed \n");
 	fdf->key_mode_axis = KEY_MODE_AXIS_X;
 }
 
 static void	ft_on_axis_y_mode_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
-		ft_putstr("ROTATE_Y MODE key pressed \n");
+		ft_putstr("AXIS_Y MODE key pressed \n");
 	fdf->key_mode_axis = KEY_MODE_AXIS_Y;
 }
 
 static void	ft_on_axis_z_mode_keypressed(t_fdf *fdf)
 {
 	if (KDEBUG)
-		ft_putstr("ROTATE_Z MODE key pressed \n");
+		ft_putstr("AXIS_Z MODE key pressed \n");
 	fdf->key_mode_axis = KEY_MODE_AXIS_Z;
 }
 
